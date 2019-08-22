333335a5ff7d501d8a8af254acf6d09af008dfc4
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-22 16:04:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,18 +2,10 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+        
+        
      }
 
 
@@ -23,38 +15,28 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
+ 		
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
+        
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+          
+      
+      }
+    programmer_hash
+    
 end
 
 def dennis_ritchies_language
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
+   
+      
+      
           :known_for => "Unix",
           :languages => ["C"]
-        }
+      
      }
+   puts  programmer_hash 
 end
 
 def adding_matz
